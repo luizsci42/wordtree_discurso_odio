ad95b6918e081db501d55a82c5e9b51e2eb5792b
luizsci42 <luizsci42@gmail.com>
2021-08-23 21:43:58 -0300

    feature/Consultando no servidor as palavras selecionadas

--- server.py
+++ server.py
@@ -3,9 +3,22 @@ from flask_cors import CORS
 import pandas as pd
 import os
 import json
+import logging
+import re
+import nltk
 
 app = Flask(__name__, static_folder='/client', static_url_path='/')
 CORS(app)
+
+
+def limpar_texto(texto):
+    texto = texto
+    texto = re.sub('\n', '', texto)
+    texto = re.sub('@\S+', '', texto)
+    texto = re.sub('#\S+', '', texto)
+    texto = re.sub(r'https://\S+', '', texto)
+
+    return texto
 
 
 @app.route('/')
@@ -16,22 +29,31 @@ def index():
 @app.route('/consultar/')
 @app.route('/consultar/<bigrama>')
 def consultar(bigrama=''):
-    path = 'res/posts_perfis_policiais.csv'
+    path = 'res/discurso_odio.csv'
     df_tweets = pd.read_csv(path, error_bad_lines=False)
-    tweets = list(df_tweets['Text'])
+    df_tweets['Texto'] = df_tweets.apply(lambda row: limpar_texto(row['Texto']), axis=1)
+
+    stopwords = nltk.corpus.stopwords.words('portuguese')
+    tweets = list(df_tweets['Texto'])
+    tweets = [palavra for palavra in tweets if not palavra.lower() in stopwords]
 
     if bigrama != '':
+        print('a palavra que chegou aqui: ' + bigrama)
         bigrama = bigrama.replace('_', ' ')
         # busca apenas as postagens que possuem o bigrama
         tweets = [post for post in tweets if (post.__contains__(bigrama))]
     else:
-        tweets = [post for post in tweets if (post.__contains__('jacarezinho'))]
+        # tweets = [post for post in tweets if (post.__contains__('jacarezinho'))]
+        tweets = [post for post in tweets if (post.__contains__('pretos'))]
 
+    logging.info('Quantidade de postagens: ', len(tweets))
     posts = {'Post': tweets}
     posts_json = json.dumps(posts, ensure_ascii=False)
     return posts_json
 
 
 if __name__ == "__main__":
+    # TODO: talvez seja necessário configurar os ambientes de dev e prod
+    #  para que as configurações de host e porta também funcionem no heroku
     port = int(os.environ.get("PORT", 5000))
     app.run(host='127.0.0.1', port=port)
